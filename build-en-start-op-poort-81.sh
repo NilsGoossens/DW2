6eb211ff7a5b619e30e48b552b9ed38abd0d0dae
Nils Goossens <147540518+NilsGoossens@users.noreply.github.com>
2025-05-14 21:42:56 +0200

    fix

--- build-en-start-op-poort-81.sh
+++ build-en-start-op-poort-81.sh
@@ -1,6 +1,4 @@
 # Docker imaga builden met naam dw2 
-docker build -t dw2.
-
+docker build -t dw2 .
 # container starten op poor 81(fort forwarding)
 docker run -dt -p 81:80 --name dw2container dw2
-
